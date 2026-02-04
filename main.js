abf3fd9ea9346bc55dd0d0a13f1cb5b112fd1dcf
LucasNS1308 <lucas123crc@gmail.com>
2026-02-04 17:16:50 -0300

    Resolução de Adicionar e remover produto

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const inputItem = document.getElementById('box'); //Criei uma variável que puxa o elemento que é descrito no input
 const btnAdd = document.querySelector('button'); //Variável que puxa o elemento button
-const list = document.getElementById('itens-list'); //Varável que puxa o elemento da Lista
+const list = document.querySelector('ul'); //Varável que puxa o elemento da Lista
 const hasTextRegex = /\d+/g; //Variável para adicionar somente letras ou palavras
+const listRemove = document.querySelector('li');
 
 inputItem.addEventListener('input', () => {
   //Adiciona um evento de clique no input para receber apenas palavras ou letras.
-
   inputItem.value = inputItem.value.replace(hasTextRegex, '');
 });
 
@@ -20,20 +20,34 @@ btnAdd.addEventListener('click', (event) => {
     return;
   }
 
-  const newItem = document.createElement('li');
+  //Cria elemento de li dentro da div itens-list
+  const newItem = document.createElement('li'); //Cria elemento de Li
+
+  //Juntar o newItem que é um elemento LI o innerHTML tem que ser igual da Li, para poder adicionar a lista
   newItem.innerHTML = `
-  <div class="">
-    <input type="checkbox">
+  <div class="container">
+    <input type="checkbox" id="item1" />
+    <label for="item1"></label>
     <span>${valorFormatado}</span>
   </div>
-  <button class="delete-button">
+  <button class="btn-remove">
     <img src="./icones/icon-delete.svg" alt="Icone para deletar produto">
   </button>
 `;
-  list.appendChild(newItem);
-  newItem.classList.add('li');
+  list.prepend(newItem); //Adiciona sempre ao primeiro elemento da lista
 
-  inputItem.value = '';
+  var btnRemove = newItem.querySelector('.btn-remove'); //Remove o item novo da lista e deixa o estático
+  btnRemove.addEventListener('click', () => {
+    newItem.remove();
+  });
+
+  inputItem.value = ''; //Limpa sempre o valor do input após ele adicionar
 
   console.log(newItem.value);
+});
+
+//remover lista estatica
+const removeLista = document.querySelectorAll('.btn-remove');
+removeLista.addEventListener('click', () => {
+  listRemove.remove();
 });
